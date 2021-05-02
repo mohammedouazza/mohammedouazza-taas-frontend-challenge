fbda7482e221f8bf6bfb7851e26e8d75ed456ea7
Mohamed Ouazza <mohamed.ouazza@cegedim-srh.com>
2021-05-02 15:22:46 +0000

    add homSelector + homeContent

---
+++ challenge-app/src/components/home/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <b-row>
+      <b-col class="columns mr-2 p-2">
+        <home-selector></home-selector>
+      </b-col>
+      <b-col class="columns text-center p-5" cols="8">
+        <home-content></home-content>
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+import HomeContentVue from "../home/HomeContent.vue";
+import HomeSelectorVue from "./HomeSelector.vue";
+export default {
+  components: {
+    "home-selector": HomeSelectorVue,
+    "home-content": HomeContentVue,
+  },
+};
+</script>
+
+<style>
+.columns {
+  border: 2px solid grey;
+  border-radius: 20px;
+  height: 90vh;
+}
+</style>
--- challenge-app/src/components/home/HomeSelector.vue
+++ challenge-app/src/components/home/HomeSelector.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     {{ getMessage }}
-    <b-button @click="authenticateGithub">Authenticate to Github</b-button>
   </div>
 </template>
 
@@ -16,13 +15,6 @@ export default {
     getMessage() {
       console.log(this.$store);
       return this.$store.getters.getHello || this.message;
-    },
-  },
-  methods: {
-    authenticateGithub() {
-      this.$store.dispatch("seyHello").then(() => {
-        this.message = this.$store.state.hello;
-      });
     },
   },
 };
